c22841535780ff60c0fc6965281ae0a5482b74f2
luiz ikeda <luizmiguelikeda11@outlook.com>
2022-12-26 15:57:44 -0300

    salvar ok

--- src/main/java/com/example/luizz/controller/IndexController.java
+++ src/main/java/com/example/luizz/controller/IndexController.java
@@ -3,17 +3,18 @@ package com.example.luizz.controller;
 import com.example.luizz.DAO.IndexDAO;
 import com.example.luizz.Model.IndexModel;
 import com.example.luizz.service.CadastroService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.awt.*;
 
 @Controller
 @RequestMapping("/index")
@@ -25,20 +26,21 @@ public class IndexController {
     @Autowired
     private CadastroService cadastroService ;
 
-    @RequestMapping("/novo")
+    @RequestMapping
     public ModelAndView novo(IndexModel indexModel) {
-        ModelAndView mv = new ModelAndView("cadastro/index");
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("indexModel", indexModel);
         return mv;
     }
 
     @PostMapping(value = "/novo")
-    public ModelAndView cadastrar(@Valid IndexModel index, BindingResult result, Model model, RedirectAttributes attributes) {
+    public ModelAndView cadastrar(@Valid IndexModel index, BindingResult result, RedirectAttributes attributes) {
         if (result.hasErrors()) {
             return novo(index);
         }
 
         cadastroService.salvar(index);
         attributes.addFlashAttribute("mensagem", "Cadastro salvo com sucesso!");
-        return new ModelAndView("redirect:/index/novo");
+        return new ModelAndView("redirect:/index");
     }
 }
